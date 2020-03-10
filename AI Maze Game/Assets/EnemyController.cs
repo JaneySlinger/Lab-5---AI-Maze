3a6411892758a281b3b2f6cfbee5676360763ca4
Unknown <janeyslinger@gmail.com>
2020-03-10 11:39:40 +0000

    Added AI navmesh to move between waypoints

---
+++ AI Maze Game/Assets/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Waypoint waypoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = waypoint.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!agent.pathPending && agent.remainingDistance < 0.5f){
+            Waypoint nextWaypoint = waypoint.nextWaypoint;
+            waypoint = nextWaypoint;
+            agent.destination = waypoint.transform.position;
+        }
+
+    }
+}
